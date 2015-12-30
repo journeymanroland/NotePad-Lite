ad3d880026efaab30b932a38607c18aad38b3c89
journeymanroland <rolandggill@gmail.com>
2015-12-29 20:55:42 -0800

    Initial Commit

---
+++ simple notepad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple notepad
+//
+//  Created by Roland Gill on 12/29/15.
+//  Copyright © 2015 Roland Gill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
